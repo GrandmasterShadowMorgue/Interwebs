5f57c7c3cac4a12cd474810f8ed7fa108a4f77d0
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-05-09 22:54:28 +0200

    Initial commit

---
+++ PeerServer.py
@@ -0,0 +1,86 @@
+#
+# PeerServer.py
+# ?
+#
+# Jonatan H Sundqvist
+# May 09 2015
+#
+
+# TODO | - Figure out API
+#        - Multiple Peers
+#        - Samples and tests
+
+# SPEC | -
+#        -
+
+
+
+import threading
+import queue
+
+
+
+class PeerServer(object):
+
+	'''
+	Docstring goes here
+
+	'''
+
+	# TODO: Separate client and server (?)
+	# TODO: Rename to Peer (?)
+
+	def __init__(self, onsend, onreceive):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+	def connect(self, IP, port):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+	def send(self, data):
+		
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+	def receive(self):
+		
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+
+def main():
+	
+	'''
+	Docstring goes here
+
+	'''
+
+	pass
+
+
+
+if __name__ == '__main__':
+	main()
