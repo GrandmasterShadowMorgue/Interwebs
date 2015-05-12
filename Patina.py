a02356219b1086e641c7853333827f9f15944740
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-05-12 03:22:49 +0200

    1) Added Qt file (future UI?) 2) Added Platformer sample

---
+++ Patina.py
@@ -0,0 +1,44 @@
+#
+# Patina
+# ?
+#
+# Jonatan H Sundqvist
+# May 12 2015
+#
+
+# TODO | - 
+#        - 
+
+# SPEC | -
+#        -
+
+
+
+from PyQt4 import QtGui
+from sys import argv
+
+
+def main():
+  
+	'''
+	Docstring goes here
+
+	'''
+
+	app = QtGui.QApplication(argv)
+
+	widget = QtGui.QWidget()
+	widget.resize(700, 400)
+	widget.move(30, 100)
+	
+	widget.setWindowTitle('Chat')
+	# widget.setWindowIcon(QtGui.QIcon('.png'))
+
+	widget.show()
+
+	app.exec_()
+
+
+
+if __name__ == '__main__':
+	main()
--- samples.py
+++ samples.py
@@ -23,6 +23,8 @@ import tkinter.ttk as ttk
 import pickle
 import time
 
+import random
+
 
 
 class ChatClient(object):
@@ -99,6 +101,109 @@ class ChatClient(object):
 
 
 
+class Platformer(object):
+
+	'''
+	Docstring goes here
+
+	'''
+
+	# TODO: Refactor (logic/graphics/network)
+	# TODO: Separate coordinate system
+
+	class Player(object):
+		# TODO: Encapsulate updates (?)
+		# TODO: Encapsulate graphical updates (?)
+		def __init__(self, name, x, y):
+			self.name = name # Name as a string
+			self.jumping = False #
+
+			self.p = x+1j*y  # Position (m   : vector)
+			self.v = 0+0j    # Velocity (m/s : vector)
+			self.m = 1       # Mass     (kg  : scalar)
+			self.f = {'gravity': 0+9.82j, 'normal': 0+9.82j} # Forces (N : vector)
+
+		def jump(self, v):
+			self.v += v
+			self.jumping = True
+			# self.f['normal'] = 0+0j
+
+		def animate(self, dt):
+			self.p += self.v * dt # TODO: Proper physics/forces/collisions/etc.
+
+
+	def __init__(self):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		# Configurations
+		self.size  = 720, 480
+		self.debug = True
+
+		# Create the window
+		self.window = tk.Tk()
+		self.window.geometry('{width}x{height}'.format(width=self.size[0], height=self.size[1]))
+		self.window.title('Platformer')
+
+		# World
+		self.groundlevel = 40
+		self.player = Player(random.choice(('Jonatan', 'Ali Baba', 'Ser Devon')), x=random.randint)
+
+		# Canvas
+		self.canvas = tk.Canvas(width=self.size[0], height=self.size[1])
+		self.ground = self.canvas.create_rectangle((0, self.size[1]-groundlevel, self.size[0], self.size[0]), fill='#45DE9F', width=0)
+		self.canvas.pack()
+
+		#
+		self.peer = Peer.Peer('localhost', 255, onreceive=lambda data: updateRemotePlayers(data)) #
+
+		#
+		self.window.mainloop()
+
+
+	def tick(self):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		self.notifyServer()
+
+
+	def notifyServer(self):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+	def updateRemotePlayers(self, data):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		pass
+
+
+
+# Sample programs
+programs = {
+	'chat':       lambda: ChatClient(), #
+	'platformer': lambda: Platformer(), #
+	'': lambda: None
+}
+
+
+
 def main():
 
 	'''
@@ -107,7 +212,8 @@ def main():
 	'''
 
 	#
-	app = ChatClient()
+	programs['platformer']()
+
 
 
 if __name__ == '__main__':
